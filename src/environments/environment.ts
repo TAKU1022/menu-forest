859da85507d20c5a9784c74b7e6dcc9bc2b75b75
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-04-27 16:58:06 +0900

    fix: indentの修正

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,14 @@ export const environment = {
   production: false,
   firebase: {
     apiKey: 'AIzaSyB06O8wT7XR0aRTnKIzk7HnbYsSr1sWwWw',
-  authDomain: 'menu-2d67e.firebaseapp.com',
-  databaseURL: 'https://menu-2d67e.firebaseio.com',
-  projectId: 'menu-2d67e',
-  storageBucket: 'menu-2d67e.appspot.com',
-  messagingSenderId: '106080504750',
-  appId: '1:106080504750:web:4fe7bb85e3c426c657163f',
-  measurementId: 'G-DRKTDKVVBC'
-  }
+    authDomain: 'menu-2d67e.firebaseapp.com',
+    databaseURL: 'https://menu-2d67e.firebaseio.com',
+    projectId: 'menu-2d67e',
+    storageBucket: 'menu-2d67e.appspot.com',
+    messagingSenderId: '106080504750',
+    appId: '1:106080504750:web:4fe7bb85e3c426c657163f',
+    measurementId: 'G-DRKTDKVVBC',
+  },
 };
 
 /*
