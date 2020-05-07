98a62d58ce0043f5bf471b2707b5fc9b11af3795
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-05-07 15:10:41 +0900

    feat(dialog): ダイヤログの見た目 #23

---
+++ src/app/login-dialog/login-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-dialog',
+  templateUrl: './login-dialog.component.html',
+  styleUrls: ['./login-dialog.component.scss'],
+})
+export class LoginDialogComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/sign-in-dialog/sign-in-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-sign-in-dialog',
+  templateUrl: './sign-in-dialog.component.html',
+  styleUrls: ['./sign-in-dialog.component.scss'],
+})
+export class SignInDialogComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
