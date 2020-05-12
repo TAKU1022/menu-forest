75a3420d42a080a6cc76c603f146c94986a011f2
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-05-12 11:51:08 +0900

    feat(dialog): SNSログイン機能実装 #24

--- src/app/account-dialog/account-dialog.component.ts
+++ src/app/account-dialog/account-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-account-dialog',
@@ -11,8 +12,54 @@ export class AccountDialogComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA)
     public data: {
       isSignUp: boolean;
-    }
+    },
+    private authSercice: AuthService,
+    private dialog: MatDialogRef<AccountDialogComponent>
   ) {}
 
   ngOnInit(): void {}
+
+  facebookLogin() {
+    this.authSercice
+      .facebookLogin()
+      .then(() => {
+        this.dialog.close();
+      })
+      .catch((error: Error) => {
+        alert(error.message);
+      });
+  }
+
+  twitterLogin() {
+    this.authSercice
+      .twrtterLogin()
+      .then(() => {
+        this.dialog.close();
+      })
+      .catch((error: Error) => {
+        alert(error.message);
+      });
+  }
+
+  gooleLogin() {
+    this.authSercice
+      .googleLogin()
+      .then(() => {
+        this.dialog.close();
+      })
+      .catch((error: Error) => {
+        alert(error.message);
+      });
+  }
+
+  yahooLogin() {
+    this.authSercice
+      .yahooLogin()
+      .then(() => {
+        this.dialog.close();
+      })
+      .catch((error: Error) => {
+        alert(error.message);
+      });
+  }
 }
