9db300e19cff19ffb019fa45203ee724d1498be9
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-06-18 15:00:29 +0900

    feat(home): 曜日ごとの献立表示 #41

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { DayMenuWithFood } from '@interfaces/my-menu';
+import { MyMenuService } from 'src/app/services/my-menu.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { switchMap } from 'rxjs/operators';
+import { User } from '@interfaces/user';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +12,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  todayMenu$: Observable<DayMenuWithFood | null> = this.authService.afUser$.pipe(
+    switchMap((user: User) => {
+      if (user) {
+        return this.myMenuService.getTodayMenu(user.uid);
+      } else {
+        return of(null);
+      }
+    })
+  );
+
+  constructor(
+    private myMenuService: MyMenuService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {}
 }
