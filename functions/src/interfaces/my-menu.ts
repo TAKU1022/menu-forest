eeecff06d6522083953e3e06d0ce5f5d49ca5642
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-06-18 14:01:40 +0900

    feat(interfaces): MyMenu Interface追加 #41

---
+++ functions/src/interfaces/my-menu.ts
@@ -0,0 +1,38 @@
+import { Food } from './food';
+
+export interface DayMenu {
+  breakfastId: string;
+  lunchId: string;
+  dinnerId: string;
+}
+
+export interface MyMenu {
+  day: {
+    sunday: DayMenu;
+    monday: DayMenu;
+    tuesday: DayMenu;
+    wednesday: DayMenu;
+    thursday: DayMenu;
+    friday: DayMenu;
+    saturday: DayMenu;
+  };
+  creatorId: string;
+  myMenuId: string;
+}
+
+export interface DayMenuWithFood {
+  breakfast: Food;
+  lunch: Food;
+  dinner: Food;
+  dayOfWeek: string;
+}
+
+export interface MyMenuWithFood {
+  sundayFood: DayMenuWithFood;
+  mondayFood: DayMenuWithFood;
+  tuesdayFood: DayMenuWithFood;
+  wednesdayFood: DayMenuWithFood;
+  thursdayFood: DayMenuWithFood;
+  fridayFood: DayMenuWithFood;
+  saturdayFood: DayMenuWithFood;
+}
