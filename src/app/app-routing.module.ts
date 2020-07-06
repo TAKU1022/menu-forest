a7fc16cef180b1f14a973b807e57bac2e11958f4
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-07-07 00:41:58 +0900

    feat(app-routing): shell追加 #76

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,44 +2,65 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AdminGuard } from './guards/admin.guard';
+import { WelcomeShellComponent } from './welcome-shell/welcome-shell.component';
+import { MainShellComponent } from './main-shell/main-shell.component';
 
 const routes: Routes = [
   {
+    path: 'welcome',
+    component: WelcomeShellComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./welcome/welcome.module').then((m) => m.WelcomeModule),
+      },
+    ],
+  },
+  {
     path: '',
-    pathMatch: 'full',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: 'menu',
-    loadChildren: () => import('./menu/menu.module').then((m) => m.MenuModule),
-  },
-  {
-    path: 'food-list',
-    loadChildren: () =>
-      import('./food-list/food-list.module').then((m) => m.FoodListModule),
-  },
-  {
-    path: 'create-my-menu',
-    loadChildren: () =>
-      import('./create-my-menu/create-my-menu.module').then(
-        (m) => m.CreateMyMenuModule
-      ),
-  },
-  {
-    path: 'setting',
-    loadChildren: () =>
-      import('./setting/setting.module').then((m) => m.SettingModule),
-  },
-  {
-    path: 'editor',
-    loadChildren: () =>
-      import('./editor/editor.module').then((m) => m.EditorModule),
-    canActivate: [AdminGuard],
-    canLoad: [AdminGuard],
-  },
-  {
-    path: '**',
-    component: NotFoundComponent,
+    component: MainShellComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        loadChildren: () =>
+          import('./home/home.module').then((m) => m.HomeModule),
+      },
+      {
+        path: 'menu',
+        loadChildren: () =>
+          import('./menu/menu.module').then((m) => m.MenuModule),
+      },
+      {
+        path: 'food-list',
+        loadChildren: () =>
+          import('./food-list/food-list.module').then((m) => m.FoodListModule),
+      },
+      {
+        path: 'create-my-menu',
+        loadChildren: () =>
+          import('./create-my-menu/create-my-menu.module').then(
+            (m) => m.CreateMyMenuModule
+          ),
+      },
+      {
+        path: 'setting',
+        loadChildren: () =>
+          import('./setting/setting.module').then((m) => m.SettingModule),
+      },
+      {
+        path: 'editor',
+        loadChildren: () =>
+          import('./editor/editor.module').then((m) => m.EditorModule),
+        canActivate: [AdminGuard],
+        canLoad: [AdminGuard],
+      },
+      {
+        path: '**',
+        component: NotFoundComponent,
+      },
+    ],
   },
 ];
 
