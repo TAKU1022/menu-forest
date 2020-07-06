44d353ee4879de2910e49c544fc5364a5eb21f7c
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-07-07 00:27:28 +0900

    feat(main-shell): main-shell追加 #76

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DrawerService } from './services/drawer.service';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +6,5 @@ import { DrawerService } from './services/drawer.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'menu';
-
-  opened: boolean;
-
-  constructor(private drawerService: DrawerService) {
-    this.drawerService.isOpen$.subscribe((opened) => (this.opened = opened));
-  }
+  constructor() {}
 }
