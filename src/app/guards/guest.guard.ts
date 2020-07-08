c3487ef35e65499f03097410339ec88592a5f6ff
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-07-08 12:54:04 +0900

    feat(guest.guard): 未認証ガード追加 #59

---
+++ src/app/guards/guest.guard.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { map, tap, take } from 'rxjs/operators';
+import { User } from '@interfaces/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GuestGuard implements CanActivate, CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authService.afUser$.pipe(
+      map((user: User) => !user),
+      tap((isGuest: boolean) => {
+        if (!isGuest) {
+          this.router.navigateByUrl('/');
+        }
+      })
+    );
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.afUser$.pipe(
+      map((user: User) => !user),
+      take(1),
+      tap((isGuest: boolean) => {
+        if (!isGuest) {
+          this.router.navigateByUrl('/');
+        }
+      })
+    );
+  }
+}
