d3ec4fbcee6f605c362ad05276245880b0936ff2
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-07-08 12:53:03 +0900

    feat(auth.guard): 認証ガード追加 #59

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { map, tap, take } from 'rxjs/operators';
+import { User } from '@interfaces/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authService.afUser$.pipe(
+      map((user: User) => !!user),
+      tap((isLogin: boolean) => {
+        if (!isLogin) {
+          this.router.navigateByUrl('/welcome');
+        }
+      })
+    );
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.afUser$.pipe(
+      map((user: User) => !!user),
+      take(1),
+      tap((isLogin: boolean) => {
+        if (!isLogin) {
+          this.router.navigateByUrl('/welcome');
+        }
+      })
+    );
+  }
+}
