38d6b9fa9cb2a392a201d7367e152f80c8ef87a3
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-07-08 12:54:38 +0900

    feat(app-routing): ガード追加 #59

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { AdminGuard } from './guards/admin.guard';
 import { WelcomeShellComponent } from './welcome-shell/welcome-shell.component';
 import { MainShellComponent } from './main-shell/main-shell.component';
+import { GuestGuard } from './guards/guest.guard';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -14,6 +16,8 @@ const routes: Routes = [
         path: '',
         loadChildren: () =>
           import('./welcome/welcome.module').then((m) => m.WelcomeModule),
+        canLoad: [GuestGuard],
+        canActivate: [GuestGuard],
       },
     ],
   },
@@ -26,16 +30,22 @@ const routes: Routes = [
         pathMatch: 'full',
         loadChildren: () =>
           import('./home/home.module').then((m) => m.HomeModule),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'menu',
         loadChildren: () =>
           import('./menu/menu.module').then((m) => m.MenuModule),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'food-list',
         loadChildren: () =>
           import('./food-list/food-list.module').then((m) => m.FoodListModule),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'create-my-menu',
@@ -43,11 +53,15 @@ const routes: Routes = [
           import('./create-my-menu/create-my-menu.module').then(
             (m) => m.CreateMyMenuModule
           ),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'setting',
         loadChildren: () =>
           import('./setting/setting.module').then((m) => m.SettingModule),
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
       {
         path: 'admin',
@@ -59,6 +73,8 @@ const routes: Routes = [
       {
         path: '**',
         component: NotFoundComponent,
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
       },
     ],
   },
