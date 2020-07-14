8d5f7c4331444538252c75a6a82177ff9b9f7fe6
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-07-14 11:27:38 +0900

    feat(welcome): MatSnackBar追加 #53

--- src/app/welcome/welcome/welcome.component.ts
+++ src/app/welcome/welcome/welcome.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -7,19 +9,44 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   loginWithFacebook() {
-    this.authService.loginWithFacebook();
+    this.authService.loginWithFacebook().then(() => {
+      setTimeout(() => {
+        this.snackBar.open('ようこそ！', null, {
+          duration: 3000,
+        });
+        this.router.navigateByUrl('/');
+      }, 4000);
+    });
   }
 
   loginWithTwitter() {
-    this.authService.loginWithTwitter();
+    this.authService.loginWithTwitter().then(() => {
+      setTimeout(() => {
+        this.snackBar.open('ようこそ！', null, {
+          duration: 3000,
+        });
+        this.router.navigateByUrl('/');
+      }, 4000);
+    });
   }
 
   loginWithGoogle() {
-    this.authService.loginWithGoogle();
+    this.authService.loginWithGoogle().then(() => {
+      setTimeout(() => {
+        this.snackBar.open('ようこそ！', null, {
+          duration: 3000,
+        });
+        this.router.navigateByUrl('/');
+      }, 4000);
+    });
   }
 }
