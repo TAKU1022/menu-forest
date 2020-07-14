d3a4df8772881954754c64fb4b04b760325d1a68
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-07-14 11:49:11 +0900

    feat(create-my-menu): my献立反映 #53

--- src/app/create-my-menu/create-my-menu.module.ts
+++ src/app/create-my-menu/create-my-menu.module.ts
@@ -5,14 +5,19 @@ import { CreateMyMenuRoutingModule } from './create-my-menu-routing.module';
 import { CreateMyMenuComponent } from './create-my-menu/create-my-menu.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ChangeMyMenuDialogComponent } from './change-my-menu-dialog/change-my-menu-dialog.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
-  declarations: [CreateMyMenuComponent],
+  declarations: [CreateMyMenuComponent, ChangeMyMenuDialogComponent],
   imports: [
     CommonModule,
     CreateMyMenuRoutingModule,
     MatButtonModule,
     MatIconModule,
+    MatDialogModule,
+    MatSnackBarModule,
   ],
 })
 export class CreateMyMenuModule {}
