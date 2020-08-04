bb55f740c8b919b93323f7df64bc8551fabdc350
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-08-04 15:37:40 +0900

    feat(user.service): メソッド追加 #52

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireStorage } from '@angular/fire/storage';
+import { User } from '@interfaces/user';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ export class UserService {
   ) {}
 
   changeUserName(userId: string, name: string): Promise<void> {
-    return this.db.doc(`users/${userId}`).update({
+    return this.db.doc<User>(`users/${userId}`).update({
       name,
     });
   }
@@ -22,8 +23,45 @@ export class UserService {
       .ref(`users/${userId}`)
       .putString(message, 'data_url');
     const avaterURL = await result.ref.getDownloadURL();
-    return this.db.doc(`users/${userId}`).update({
+    return this.db.doc<User>(`users/${userId}`).update({
       avaterURL,
+    });
+  }
+
+  initializeUserIsEatenFood(userId: string): Promise<void> {
+    return this.db.doc<User>(`users/${userId}`).update({
+      isEatenBreakfast: false,
+      isEatenLunch: false,
+      isEatenDinner: false,
+    });
+  }
+
+  changeUserIsEatenFood(
+    userId: string,
+    time: number,
+    isEaten: boolean
+  ): Promise<void> | void {
+    switch (time) {
+      case 0:
+        return this.db.doc<User>(`users/${userId}`).update({
+          isEatenBreakfast: isEaten,
+        });
+      case 1:
+        return this.db.doc<User>(`users/${userId}`).update({
+          isEatenLunch: isEaten,
+        });
+      case 2:
+        return this.db.doc<User>(`users/${userId}`).update({
+          isEatenDinner: isEaten,
+        });
+      default:
+        return alert('失敗');
+    }
+  }
+
+  changeEatCount(userId: string, eatCount: number): Promise<void> {
+    return this.db.doc<User>(`users/${userId}`).update({
+      eatCount,
     });
   }
 }
