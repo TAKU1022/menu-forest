302c772eb3f95f1bdf1add89cec03556e2703f2a
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-09-23 14:27:47 +0900

    feat(menu): メニュー画像表示 #51

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MenuRoutingModule } from './menu-routing.module';
 import { MenuComponent } from './menu/menu.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [MenuComponent],
-  imports: [CommonModule, MenuRoutingModule],
+  imports: [CommonModule, MenuRoutingModule, MatCardModule],
 })
 export class MenuModule {}
