f8972f858fee3fdee873899e3d764a0f37528b06
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-10-29 17:13:01 +0900

    feat(user): デザイン変更 #68

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,17 +4,10 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user/user.component';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [UserComponent],
-  imports: [
-    CommonModule,
-    UserRoutingModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-  ],
+  imports: [CommonModule, UserRoutingModule, MatButtonModule, MatIconModule],
 })
 export class UserModule {}
--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -5,6 +5,8 @@ import { PostWithFoodWithUser } from '@interfaces/post';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap, tap, take } from 'rxjs/operators';
 import { LoadingService } from 'src/app/services/loading.service';
+import { User } from '@interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -12,18 +14,32 @@ import { LoadingService } from 'src/app/services/loading.service';
   styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
+  user$: Observable<User>;
   userPosts$: Observable<PostWithFoodWithUser[]>;
 
   constructor(
     private postService: PostService,
     private route: ActivatedRoute,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private userService: UserService
   ) {
     this.loadingService.toggleLoading(true);
   }
 
   ngOnInit(): void {
+    this.getUser();
     this.getUserPosts();
+  }
+
+  private getUser(): void {
+    this.user$ = this.route.queryParamMap.pipe(
+      switchMap((param: ParamMap) => {
+        const userId = param.get('id');
+        return this.userService.getUserbyId(userId);
+      }),
+      take(1),
+      tap(() => this.loadingService.toggleLoading(false))
+    );
   }
 
   private getUserPosts(): void {
@@ -32,8 +48,7 @@ export class UserComponent implements OnInit {
         const userId = param.get('id');
         return this.postService.getUserPosts(userId);
       }),
-      take(1),
-      tap(() => this.loadingService.toggleLoading(false))
+      take(1)
     );
   }
 }
