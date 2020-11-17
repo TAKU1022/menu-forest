668e3fb835f192a8fa7667007ca0096e4f3aa407
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-11-17 16:49:10 +0900

    feat(usage): 使い方説明画面追加 #66

---
+++ src/app/usage/usage-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UsageComponent } from './usage/usage.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: UsageComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class UsageRoutingModule {}
