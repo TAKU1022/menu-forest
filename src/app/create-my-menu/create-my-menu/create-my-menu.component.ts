1a1f84ffea062b9b831c380a711f151dcc3db323
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-11-25 15:15:08 +0900

    feat(create-my-menu): チュートリアル追加 #66

--- src/app/create-my-menu/create-my-menu/create-my-menu.component.ts
+++ src/app/create-my-menu/create-my-menu/create-my-menu.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { MyMenuService } from 'src/app/services/my-menu.service';
 import { DayMenuWithFood, MyMenu } from '@interfaces/my-menu';
 import { Observable, Subscription } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { Food } from '@interfaces/food';
-import { tap } from 'rxjs/operators';
+import { take, tap } from 'rxjs/operators';
 import { LoadingService } from 'src/app/services/loading.service';
 import { ChangeMyMenuDialogComponent } from 'src/app/change-my-menu-dialog/change-my-menu-dialog/change-my-menu-dialog.component';
 import { PostDialogComponent } from '../post-dialog/post-dialog.component';
+import { TutorialService } from 'src/app/services/tutorial.service';
+import { User } from '@interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-create-my-menu',
   templateUrl: './create-my-menu.component.html',
   styleUrls: ['./create-my-menu.component.scss'],
 })
-export class CreateMyMenuComponent implements OnInit, OnDestroy {
+export class CreateMyMenuComponent implements OnInit, OnDestroy, AfterViewInit {
   private userId: string = this.authService.userId;
   private subscription: Subscription;
+  private user$: Observable<User> = this.authService.user$;
 
   weekMenu$: Observable<
     DayMenuWithFood[]
@@ -30,7 +34,9 @@ export class CreateMyMenuComponent implements OnInit, OnDestroy {
     private authService: AuthService,
     private myMenuService: MyMenuService,
     private dialog: MatDialog,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private tutorialService: TutorialService,
+    private userService: UserService
   ) {
     this.loadingService.toggleLoading(true);
   }
@@ -41,6 +47,22 @@ export class CreateMyMenuComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  ngAfterViewInit(): void {
+    this.startTutorial();
+  }
+
+  private startTutorial(): void {
+    this.user$
+      .pipe(take(1))
+      .toPromise()
+      .then((user: User) => {
+        if (!user.isCompletedCreateMyMenuTutorial) {
+          this.tutorialService.startCreateMyMenuTutorial();
+          this.userService.completeTutorial('create-my-menu', this.userId);
+        }
+      });
   }
 
   private getMyMenu(): void {
