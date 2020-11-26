fa8834333f19d7aa80537b9e4499d7f9e9d48f93
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-11-26 11:36:28 +0900

    feat(my-menu-loading): 離脱ガード追加 #66

--- src/app/my-menu-loading/my-menu-loading-routing.module.ts
+++ src/app/my-menu-loading/my-menu-loading-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyMenuLoadingGuard } from '../guards/my-menu-loading.guard';
 import { MyMenuLoadingComponent } from './my-menu-loading/my-menu-loading.component';
 
 const routes: Routes = [
@@ -7,6 +8,7 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: MyMenuLoadingComponent,
+    canDeactivate: [MyMenuLoadingGuard],
   },
 ];
 
