9060ff65ce2152fca9c62d965a0062c9532dc7e0
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-11-29 15:19:01 +0900

    feat(my-page): マイページ画面無限スクロール追加 #122

--- src/app/my-page/my-page.module.ts
+++ src/app/my-page/my-page.module.ts
@@ -5,9 +5,18 @@ import { MyPageRoutingModule } from './my-page-routing.module';
 import { MyPageComponent } from './my-page/my-page.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [MyPageComponent],
-  imports: [CommonModule, MyPageRoutingModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MyPageRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    InfiniteScrollModule,
+    MatProgressSpinnerModule,
+  ],
 })
 export class MyPageModule {}
--- src/app/my-page/my-page/my-page.component.ts
+++ src/app/my-page/my-page/my-page.component.ts
@@ -4,6 +4,9 @@ import { Observable } from 'rxjs';
 import { User } from '@interfaces/user';
 import { PostService } from 'src/app/services/post.service';
 import { PostWithFoodWithUser } from '@interfaces/post';
+import { take, tap } from 'rxjs/operators';
+import { LoadingService } from 'src/app/services/loading.service';
+import { firestore } from 'firebase';
 
 @Component({
   selector: 'app-my-page',
@@ -12,16 +15,50 @@ import { PostWithFoodWithUser } from '@interfaces/post';
 })
 export class MyPageComponent implements OnInit {
   private userId: string = this.authService.userId;
+  private lastPostDocument: firestore.QueryDocumentSnapshot<
+    firestore.DocumentData
+  >;
+  private isInitial = true;
 
-  user$: Observable<User> = this.authService.user$;
-  userPosts$: Observable<
-    PostWithFoodWithUser[]
-  > = this.postService.getUserPosts(this.userId);
+  user$: Observable<User> = this.authService.user$.pipe(
+    tap(() => this.loadingService.toggleLoading(false))
+  );
+  myPosts: PostWithFoodWithUser[] = [];
+  isLoading: boolean;
 
   constructor(
     private authService: AuthService,
-    private postService: PostService
-  ) {}
+    private postService: PostService,
+    private loadingService: LoadingService
+  ) {
+    this.loadingService.toggleLoading(true);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getMyPosts();
+  }
+
+  getMyPosts(): void {
+    this.postService
+      .getUserPosts(this.userId, this.lastPostDocument)
+      .pipe(
+        take(1),
+        tap((userPost) => {
+          this.isLoading = true;
+        })
+      )
+      .toPromise()
+      .then(({ postsWithFoodWithUser, lastPostDocument }) => {
+        setTimeout(
+          () => {
+            this.myPosts.push(...postsWithFoodWithUser);
+            this.lastPostDocument = lastPostDocument;
+            this.isInitial = false;
+            this.isLoading = false;
+          },
+          this.isInitial ? 0 : 1500
+        );
+      })
+      .catch(() => (this.isLoading = false));
+  }
 }
