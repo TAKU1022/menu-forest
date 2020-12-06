21c7e8974f886d934bbe5287664bd846e2269858
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-12-06 16:19:45 +0900

    refactor(menu): 不要コード削除 #90

--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
-import { FoodDetailComponent } from '../food-detail/food-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: MenuComponent,
-  },
-  {
-    path: ':detail',
-    component: FoodDetailComponent,
   },
 ];
 
