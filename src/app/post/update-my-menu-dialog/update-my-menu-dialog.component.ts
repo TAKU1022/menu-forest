5d72ae130a0d2e984fd58315bc7f964cbad496a8
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-12-09 17:45:30 +0900

    feat(update-my-menu-dialog): My献立上書き機能追加 #130

--- src/app/post/update-my-menu-dialog/update-my-menu-dialog.component.ts
+++ src/app/post/update-my-menu-dialog/update-my-menu-dialog.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { DayMenu } from '@interfaces/my-menu';
+import { MyMenuService } from 'src/app/services/my-menu.service';
 
 @Component({
   selector: 'app-update-my-menu-dialog',
@@ -6,7 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-my-menu-dialog.component.scss'],
 })
 export class UpdateMyMenuDialogComponent implements OnInit {
-  constructor() {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    private data: {
+      myMenuId: string;
+      postFoods: {
+        sunday: DayMenu;
+        monday: DayMenu;
+        tuesday: DayMenu;
+        wednesday: DayMenu;
+        thursday: DayMenu;
+        friday: DayMenu;
+        saturday: DayMenu;
+      };
+    },
+    private myMenuService: MyMenuService,
+    private snackBar: MatSnackBar,
+    private router: Router,
+    private dialogRef: MatDialogRef<UpdateMyMenuDialogComponent>
+  ) {}
 
   ngOnInit(): void {}
+
+  changeMyMenuToUserMenu(): void {
+    this.myMenuService
+      .changeMyMenuToUserMenu(this.data.myMenuId, this.data.postFoods)
+      .then(() => {
+        this.dialogRef.close();
+        this.snackBar.open('この献立をMy献立に登録しました！', null);
+        this.router.navigateByUrl('/create-my-menu');
+      });
+  }
 }
